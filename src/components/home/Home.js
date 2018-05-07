c61d4980484fb890df3b4397d8163cec6e78b60c
originalhumanbeing <han.hyunjung17@gmail.com>
2018-05-07 22:39:44 +0900

    Render each item's main image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,63 +6,76 @@ import {GridList, GridTile} from 'material-ui/GridList';
 
 const varietyShowCategories = [
     {
-        title: "카테고리 1",
-        items : [
+        title: "여행 가고 싶을 때",
+        items: [
             {
-                title : "예능 1"
+                title: "신서유기",
+                src: "/test_images/sinseoyugi.jpg"
             },
             {
-                title : "예능 2"
+                title: "정글의 법칙",
+                src: "/test_images/jungle.jpg"
             },
             {
-                title : "예능 3"
+                title: "배틀트립",
+                src: "/test_images/battletrip.jpg"
             },
             {
-                title : "예능 4"
+                title: "1박 2일",
+                src: "/test_images/2nights1day.jpg"
             },
             {
-                title : "예능 5"
-            },
-            {
-                title : "예능 6"
-            },
-            {
-                title : "예능 7"
-            },
-            {
-                title : "예능 8"
+                title: "뭉쳐야 뜬다",
+                src: "/test_images/mungcheoyaddunda.jpg"
             }
         ]
     },
     {
-        title: "카테고리 2",
-        items : [
+        title: "먹고 사는 이야기",
+        items: [
             {
-                title : "예능 1"
+                title: "나 혼자 산다",
+                src: "/test_images/nahonjasanda.jpg"
             },
             {
-                title : "예능 2"
+                title: "전지적 참견 시점",
+                src: "/test_images/jeonjijeokchamgeon.jpg"
             },
             {
-                title : "예능 3"
+                title: "이불 밖은 위험해",
+                src: "/test_images/ebulbakeunwiheomhae.jpg"
             },
             {
-                title : "예능 4"
+                title: "냉장고를 부탁해",
+                src: "/test_images/nangjanggo.jpg"
             },
             {
-                title : "예능 5"
-            },
-            {
-                title : "예능 6"
-            },
-            {
-                title : "예능 7"
-            },
-            {
-                title : "예능 8"
+                title: "효리네 민박",
+                src: "/test_images/hyorineminbak.jpg"
             }
         ]
-    }
+    },
+    {
+        title: "음악이 듣고 싶을 때",
+        items: [
+            {
+                title: "복면가왕",
+                src: "/test_images/bokmyeongawang.jpg"
+            },
+            {
+                title: "M COUNT DOWN",
+                src: "/test_images/mcountdown.jpg"
+            },
+            {
+                title: "주간아이돌",
+                src: "/test_images/weeklyidol.jpg"
+            },
+            {
+                title: "투유 프로젝트 - 슈가맨2",
+                src: "/test_images/toyouproject.jpg"
+            }
+        ]
+    },
 ];
 
 const styles = {
@@ -90,7 +103,7 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        this.setState({'visibleEpiBlockFlags': varietyShowCategories.map(()=> false)});
+        this.setState({'visibleEpiBlockFlags': varietyShowCategories.map(() => false)});
         console.log(this.state.visibleEpiBlockFlags);
     }
 
@@ -98,6 +111,8 @@ class Home extends Component {
         return () => {
             let visibleEpiBlockFlags = this.state.visibleEpiBlockFlags;
             visibleEpiBlockFlags[cate_idx] = true;
+            //cate_idx가 들어가는 이유: 어떤 쇼를 택하든 밑에 박스가 나오는 것은 동일
+            //그 박스 안에 들어가는 내용만 바꿔서 렌더링 해주면 되니까 꼭 show_idx를 전달하지 않아도 됌
             this.setState({'visibleEpiBlockFlags': visibleEpiBlockFlags});
             console.log(cate_idx, show_idx, this.state.visibleEpiBlockFlags)
         }
@@ -115,13 +130,20 @@ class Home extends Component {
                             <h3>{cate.title}</h3>
                             <GridList cellHeight={280} cols={2.2} key={idx}
                                       style={styles.gridList}>
-                                {cate.items.map((show, show_idx) =>
-                                    <GridTile key={show_idx} title={show.title} onClick={this.onClickVarietyShowElement(idx, show_idx)}>
-                                        <img src="/test_images/sinseoyugi.jpg" alt=""/>
+                                {cate.items.map((cate, cate_idx, cate_src) =>
+                                    <GridTile key={cate_idx} title={cate.title}
+                                              onClick={this.onClickVarietyShowElement(idx, cate_idx)}>
+                                        <img src={cate.src} alt=""/>
                                     </GridTile>
                                 )}
                             </GridList>
-                            {this.state.visibleEpiBlockFlags[idx] ? <div>hide</div> : ''}
+                            {this.state.visibleEpiBlockFlags[idx] ?
+                                <div style={styles.root}>
+                                    <h4>episode title</h4>
+                                    <GridList cellHeight={280} cols={2.2} key={1} style={styles.gridList}>
+                                        <GridTile>episode here!</GridTile>
+                                    </GridList>
+                                </div> : ''}
                         </div>
                     )}
                 </div>
